330d7bcdab6c1ac66400856703153b4cc27d0327
Brian Cristante <brian.cristante@gmail.com>
2018-03-27 20:21:51 -0400

    Adding largely untested error checking by following the documentation

--- win64/synchronization.hpp
+++ win64/synchronization.hpp
@@ -3,32 +3,63 @@
 #include <optional>
 #include <vector>
 
-#include <Windows.h>
+#include "error.hpp"
 
 namespace windows
 {
     namespace synchronization
     {
-        template <typename T>
-        void wait_for_all(const std::vector<T>& objects, unsigned long timeout = INFINITE)
+        struct timeout_wexception : public windows::wexception {};
+
+        inline void wait_for_all(const std::vector<HANDLE>& objects, unsigned long timeout = INFINITE)
         {
-            // TODO error handling
             const auto events = ::WaitForMultipleObjects(
                 objects.size(),
                 objects.data(),
                 true,
                 timeout);
+
+            if (events < WAIT_ABANDONED_0 + objects.size())
+            {
+                // All objects signaled (possibly with an abandoned mutex)
+                return;
+            }
+            else if (events == WAIT_TIMEOUT)
+            {
+                throw timeout_wexception{};
+            }
+            else
+            {
+                throw windows::win32_exception{ ::GetLastError() };
+            }
         }
 
-        template <typename T>
-        void wait_for_any(const std::vector<T>& objects, unsigned long timeout = INFINITE)
+        //! Wait on a `vector` of objects and return the first object that signals
+        //! (abandoned mutexes also count).
+        inline HANDLE wait_for_any(const std::vector<HANDLE>& objects, unsigned long timeout = INFINITE)
         {
-            // TODO error handling
             const auto events = ::WaitForMultipleObjects(
                 objects.size(),
                 objects.data(),
                 false,
                 timeout);
+
+            if (events < WAIT_OBJECT_0 + objects.size())
+            {
+                return objects[events - WAIT_OBJECT_0];
+            }
+            else if (events < WAIT_ABANDONED_0 + objects.size())
+            {
+                return objects[events - WAIT_ABANDONED_0];
+            }
+            else if (events == WAIT_TIMEOUT)
+            {
+                throw timeout_wexception{};
+            }
+            else
+            {
+                throw windows::win32_exception{ ::GetLastError() };
+            }
         }
     }
 }
