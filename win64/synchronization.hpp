0490dd2b8cb65b955f0767e3adf59ee3b0e9d74c
Brian Cristante <brian.cristante@gmail.com>
2018-03-27 20:47:11 -0400

    Add `wait` function

--- win64/synchronization.hpp
+++ win64/synchronization.hpp
@@ -11,6 +11,26 @@ namespace windows
     {
         struct timeout_wexception : public windows::wexception {};
 
+        inline void wait(HANDLE object, unsigned long timeout = INFINITE)
+        {
+            const auto result = ::WaitForSingleObject(object, timeout);
+
+            if (result == WAIT_ABANDONED)
+            {
+                // TODO we lose information here
+                // caller needs to know that the mutex was abandoned
+                return;
+            }
+            else if (result == WAIT_TIMEOUT)
+            {
+                throw timeout_wexception{};
+            }
+            else if (result == WAIT_FAILED)
+            {
+                throw windows::win32_wexception{ ::GetLastError() };
+            }
+        }
+
         inline void wait_for_all(const std::vector<HANDLE>& objects, unsigned long timeout = INFINITE)
         {
             const auto events = ::WaitForMultipleObjects(
@@ -50,6 +70,8 @@ namespace windows
             }
             else if (events < WAIT_ABANDONED_0 + objects.size())
             {
+                // TODO we lose information here
+                // caller needs to know that the mutex was abandoned
                 return objects[events - WAIT_ABANDONED_0];
             }
             else if (events == WAIT_TIMEOUT)
